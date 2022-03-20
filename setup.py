b3b72babbecd09d49597f462c376608294bcb347
Ege Sabanci <egesabanci@outlook.com.tr>
2022-03-21 00:53:38 +0300

    create: initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import find_packages, setup
+
+
+def get_readme() -> str:
+  pass
+
+
+def get_dependencies() -> list:
+  pass
+
+
+name = "warfle"
+version = "0.0.1"
+packages = find_packages()
+
+
+setup = (
+  name = name,
+  version = version,
+  packages = packages
+)
